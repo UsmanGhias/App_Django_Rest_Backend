02e64d9cc2b63f1712a940f3b9ba88d4e7079f77
UsmanGhias <bsef20m540@pucit.edu.pk>
2024-01-04 11:59:56 +0500

    Alhamdulillah Resolved Issue

--- service/settings.py
+++ service/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "storages",
+    "leaflet",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "api.apps.ApiConfig",
